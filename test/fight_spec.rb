f5e16cdcf6e8b7e45aec06476623b2f9cea9e88b
itgsod-jannike-rivelsrud <jannike.rivelsrud@elev.it-gymnasiet.se>
2013-11-08 11:09:33 +0100

    added hp and weapon

---
+++ test/fight_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require_relative '../lib/fight'
+require_relative '../lib/warrior'
+
+describe 'Starting a new fight' do
+
+  it 'should have 2 participants' do
+    f = Fight.new("", "")
+    f.participants.count.should == 2
+  end
+
+  it 'should have warriors as participants' do
+    f = Fight.new(SpearWarrior.new("H"), SwordWarrior.new("O"))
+    f.participants.first.is_a?(Warrior).should == true
+    f.participants.last.is_a?(Warrior).should == true
+  end
+end
+
+#describe 'turns' do
+#
+#  it 'should let the Warriors punch each other' do
+#    f = Fight.new(SpearWarrior.new("J"), SwordWarrior.new("L"))
+#  end
+end
