1a7c85fae2c927844918e02b6a7171ebb646680a
itgsod-jannike-rivelsrud <jannike.rivelsrud@elev.it-gymnasiet.se>
2013-11-08 13:47:42 +0100

    Refactored Warrior and added Weapon

--- test/warrior_spec.rb
+++ test/warrior_spec.rb
@@ -1,57 +1,31 @@
 require 'rspec'
 require_relative '../lib/warrior'
+require_relative '../lib/weapon'
 
 describe 'Warrior' do
 
-  it 'should have the right name' do
-    w = Warrior.new('Simba', 0)
-    w.name.should == 'Simba'
-
+  it 'should have a name' do
+    w = Warrior.new("Ola", nil, nil)
+    w.name.should == "Ola"
   end
 
-  it 'should not have a weapon' do
-    w = Warrior.new("P", 0)
-    expect { w.weapon }.to raise_error NotImplementedError
+  it 'should have hp' do
+    Warrior.new(nil, 10, nil).hp.should == 10
+    Warrior.new(nil, 15, nil).hp.should == 15
   end
 
-  it 'should not have hp' do
-    w = Warrior.new("", 0)
-    expect {w.hp}.to raise_error NotImplementedError
-  end
-end
-
-describe 'SpearWarrior' do
-
-  it 'should have a name and a spear' do
-    sw = SpearWarrior.new("Scar")
-    sw.name.should == "Scar"
-    sw.weapon.should == "Spear"
-  end
-
-  it 'should have 10 hp' do
-    sw = SpearWarrior.new("")
-    sw.hp.should == 10
-  end
-end
-
-describe "SwordWarrior" do
-
-  it 'should have a name and a Sword' do
-    sw = SwordWarrior.new("Scar")
-    sw.name.should == "Scar"
-    sw.weapon.should == "Sword"
-  end
-
-  it 'should have 15 hp' do
-    sw = SwordWarrior.new("")
-    sw.hp.should == 15
+  it 'should have a weapon' do
+    weapon1 = Weapon.new("Rusty Sword", 3, 6)
+    weapon2 = Weapon.new("Long Spear", 2, 7)
+    Warrior.new(nil, nil, weapon1).weapon.should == weapon1
+    Warrior.new(nil, nil, weapon2).weapon.should == weapon2
   end
 end
 
 describe 'Attacking' do
 
   it 'should be able to attack' do
-    sw = SwordWarrior.new("Kalle")
+    sw = Warrior.new(nil, nil, nil)
     sw.should respond_to(:attack).with(1).arguments
   end
 end
@@ -59,17 +33,23 @@ end
 describe 'Taking hits' do
 
   it 'should be able to take hits' do
-    sw = SwordWarrior.new('')
-    sw.should respond_to(:take_hit)
+    sw = Warrior.new(nil, nil, nil)
+    sw.should respond_to(:take_hit).with(1).arguments
   end
 
-
   it 'should be able to take a hit from an attacker' do
-    attacker = SwordWarrior.new('')
-    defender = SwordWarrior.new('')
+    attacker = Warrior.new('', nil, nil)
+    defender = Warrior.new('', nil, nil)
 
     defender.should_receive(:take_hit)
     attacker.attack(defender)
+  end
 
+  it 'should take dmg from the attacking weapon' do
+    attacker = Warrior.new(nil, nil, Weapon.new("Big Sword", 4, 10))
+    defender = Warrior.new(nil, 15, nil)
+
+    attacker.attack(defender)
+    defender.hp.should be <= 11
   end
 end
