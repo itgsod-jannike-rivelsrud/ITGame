15c0c4c7b3bb4c07de05f0dba9c31f4fc3bf2c4a
itgsod-jannike-rivelsrud <jannike.rivelsrud@elev.it-gymnasiet.se>
2013-11-08 10:48:43 +0100

    added hp and weapon

--- test/warrior_spec.rb
+++ test/warrior_spec.rb
@@ -3,13 +3,47 @@ require_relative '../lib/warrior'
 
 describe 'Warrior' do
 
-  it 'should have the right name and weapon' do
-    w = Warrior.new("Simba", "Sword")
-    w.weapon.should == "Sword"
-    w.name.should == "Simba"
-    w1 = Warrior.new("Kyle", "Hammer")
-    w1.name.should == "Kyle"
-    w1.weapon.should == "Hammer"
+  it 'should have the right name' do
+    w = Warrior.new('Simba')
+    w.name.should == 'Simba'
 
+  end
+
+  it 'should not have a weapon' do
+    w = Warrior.new("P")
+    expect { w.weapon }.to raise_error NotImplementedError
+  end
+
+  it 'should not have hp' do
+    w = Warrior.new("")
+    expect {w.hp}.to raise_error NotImplementedError
+  end
+end
+
+describe 'SpearWarrior' do
+
+  it 'should have a name and a spear' do
+    sw = SpearWarrior.new("Scar")
+    sw.name.should == "Scar"
+    sw.weapon.should == "Spear"
+  end
+
+  it 'should have 10 hp' do
+    sw = SpearWarrior.new("")
+    sw.hp.should == 10
+  end
+end
+
+describe "SwordWarrior" do
+
+  it 'should have a name and a Sword' do
+    sw = SwordWarrior.new("Scar")
+    sw.name.should == "Scar"
+    sw.weapon.should == "Sword"
+  end
+
+  it 'should have 15 hp' do
+    sw = SwordWarrior.new("")
+    sw.hp.should == 15
   end
 end
